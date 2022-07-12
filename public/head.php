e997b1e57148043c4e48b88704f0f9ae91ffbe8c
Debora-Mind <debora.almeida.de.mello@gmail.com>
2022-07-11 23:10:04 -0300

    Icones navbar
    Ícones do navbar padronizados.

--- public/head.php
+++ public/head.php
@@ -6,5 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="assets/bootstrap.css">
     <link rel="stylesheet" href="assets/layot.css">
+    <script src="https://kit.fontawesome.com/1b1e4ff655.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 </head>
